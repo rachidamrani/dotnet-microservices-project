39886ce9b721edceba1fcd94a2fc43c5bbf4a1b1
Rachid <rachid.amrani.alaoui@inaxel.com>
2025-07-26 18:54:23 +0200

    Implémente la gestion globale des exceptions.
    Implémente un middleware pour la gestion centralisée des exceptions non gérées.
    
    Ce middleware intercepte les exceptions, les enregistre dans les logs avec le type et le message (y compris l'inner exception si présente),
    et retourne une réponse JSON standardisée avec un code d'erreur 500.
    
    L'extension `UseExceptionHandlingMiddleware` facilite l'intégration du middleware dans le pipeline HTTP.

---
+++ eCommerce.Solution.UsersService/eCommerce.API/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,35 @@
+﻿namespace eCommerce.API.Middlewares;
+public class ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
+{
+    public async Task Invoke(HttpContext httpContext)
+    {
+        try
+        {
+            await next(httpContext);
+        }
+        catch (Exception ex)
+        {
+            // Log the exception type and message
+            logger.LogError($"{ex.GetType().ToString()}: {ex.Message}");
+
+            if (ex.InnerException is not null)
+            {
+                // Log the inner exception type and message
+                logger.LogError($"{ex.InnerException.GetType().ToString()}: {ex.InnerException.Message}");
+            }
+
+            httpContext.Response.StatusCode = 500; //Internal Server Error
+            await httpContext.Response.WriteAsJsonAsync(new { Message = ex.Message, Type = ex.GetType().ToString() });
+        }
+
+    }
+}
+
+// Extension method used to add the middleware to the HTTP request pipeline.
+public static class ExceptionHandlingMiddlewareExtensions
+{
+    public static IApplicationBuilder UseExceptionHandlingMiddleware(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<ExceptionHandlingMiddleware>();
+    }
+}
